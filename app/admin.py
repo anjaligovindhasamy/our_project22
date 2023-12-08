0f4f424585cf07dc8e91365a1521b9a81680c19b
anjaligovindhasamy <anjaligovindhasamy>
2023-12-08 17:35:38 +0530

    first commit

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from app.models import AccessRecord,Topic,Webpage
+
+# Register your models here.
+
+admin.site.register(Topic)
+
+admin.site.register(Webpage)
+
+admin.site.register(AccessRecord)
