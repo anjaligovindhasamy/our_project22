6b5d654c9202e7410771a677133454cd85d7be74
anjaligovindhasamy <anjaligovindhasamy>
2024-01-11 23:01:20 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from app.models import *
 
 # Create your views here.
+def display_topic(request):
+    QLTO=Topic.objects.all()
+    d={'topic':QLTO}
+    return render(request,'display_topic.html',d)
+
+def webpage(request):
+    QLTO=Webpage.objects.all()
+    d={'webpage':QLTO}
+    return render(request,'webpage.html',d)
+
+def access(request):
+    QLTO=AccessRecord.objects.all()
+    d={'access':QLTO}
+    return render(request,'access.html',d)
+def insert_topic(request):
+    to=input('enter the topic_name')
+    NTO=Topic.objects.get_or_create(topic_name=to)[0]
+    NTO.save()
+
+    QLTO=Topic.objects.all()
+    d={'topic':QLTO}
+    return render(request,'display_topic.html',d)
+
+def insert_webpage(request):
+    to=input('enter topic_name')
+    no=input('enter the name')
+    uo=input('enter the url')
+    NTO=Topic.objects.get(topic_name=to)
+    NWO=Webpage.objects.get_or_create(topic_name=NTO,name=no,url=uo)[0]
+    NWO.save()
+
+    QLTO=Webpage.objects.all()
+    d={'webpage':QLTO}
+    return render(request,'webpage.html',d)
+
+def insert_access(request):
+    PK=int(input('enter the pk webpage'))
+    do=input('enter the date')
+    ao=input('enter the author')
+    NWO=Webpage.objects.get(pk=PK)
+    NAO=AccessRecord.objects.get_or_create(name=NWO,date=do,author=ao)[0]
+    NAO.save()
+
+
+    QLTO=AccessRecord.objects.all()
+    d={'access':QLTO}
+    return render(request,'access.html',d)
--- project22/settings.py
+++ project22/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'project22.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project22.wsgi.application'
+
 
 
 # Database
